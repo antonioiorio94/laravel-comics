f8d0071b73d477b29d786dc392992577bbb2bc61
Antonio Iorio <antonioiorio94@gmail.com>
2022-05-19 16:50:33 +0200

    aggiunta visualizzazione comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -5,7 +5,25 @@ DC Comics - Comics
 @endsection
 
 @section('content')
-<div>
-  @dump($comics)
+<div class="container">
+    
+    @if(count($comics) > 0)
+    <div class="card-container">
+      @foreach ($comics as $item)
+        <div class="card">
+          <a href="#">
+            <div class="cover">
+              <img src="{{$item['thumb']}}" alt="{{$item['series']}}">
+            </div>
+            <p>{{$item['series']}}</p>
+            
+
+          </a>
+        </div>
+
+          
+      @endforeach
+    </div>
+    @endif
 </div>
 @endsection
